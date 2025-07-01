fee484cefd41cb36683f6bf3b6b03154235cf3b1
ziad10100 <ziad.mahmoud151@gmail.com>
2025-07-01 17:27:19 +0300

    mark as seen

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import { GiHamburgerMenu } from 'react-icons/gi'
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { FaUser } from "react-icons/fa";
-
 
 export default function Navbar({ isVisable, handleToggle }) {
   return (
     <div className="navbar flex text-black items-center justify-between  p-4 rounded-md mb-5 shadow-md  hover:shadow-xl transition duration-300 sticky top-0 z-2000 bg-stone-100">
-      {!isVisable&&
-                <div className="text-[1.5rem] flex items-center gap-5">
-
-                     <span className="text-[1.5rem] text-black cursor-pointer hover:scale-125 transition duration-300" onClick={handleToggle} >
-                          <GiHamburgerMenu className="text-2xl" />
-                    </span>
-                    <div className='logo cursor-pointer'><span className='font-bold'>VEE </span> MANAGE</div>
-                </div>
-            }
+      {!isVisable && (
+        <div className="text-[1.5rem] flex items-center gap-5">
+          <span
+            className="text-[1.5rem] text-black cursor-pointer hover:scale-125 transition duration-300"
+            onClick={handleToggle}
+          >
+            <GiHamburgerMenu className="text-2xl" />
+          </span>
+          <div className="logo cursor-pointer">
+            <span className="font-bold">VEE </span> MANAGE
+          </div>
+        </div>
+      )}
       <div className="user-info flex items-center gap-3 ml-auto">
-        <span><FaUser/></span>
-        <span className="">{localStorage.getItem("displayName")}</span>
+        <span>
+          <FaUser />
+          
+        </span>
+        <span className="">Ziad Mahmoud</span>
       </div>
     </div>
-  )
+  );
 }
