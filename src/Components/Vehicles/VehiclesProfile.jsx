2fca5be29a857ec71665c81ac06c9653d23802a8
osama elnahhas <osamaelnahas43@gmail.com>
2025-07-07 00:34:36 +0300

    starting profile vehicle

--- src/Components/Vehicles/VehiclesProfile.jsx
+++ src/Components/Vehicles/VehiclesProfile.jsx
@@ -29,7 +29,42 @@ export default function VehiclesProfile() {
     data&&console.log("Vehicles data:", data);
 
   return <>
-  <Profile />
+  <div className='bg-white p-4 rounded-md shadow-md '>
+    <div className="title flex justify-between p-2 border-b-2 border-gray-200">
+      <div className="modelName text-blue-500 font-bold text-2xl"> { data?.vehicleModel?.brand?.name } { data?.vehicleModel?.name} </div>
+      <div className={`status ${data?.status === "Available" ? "bg-green-500" : "bg-red-500"}  text-white p-2 rounded-2xl px-5 font-bold`}>{data?.status}</div>
+    </div>
+    <div className="details grid md:grid-cols-2 p-2">
+      <div className="parentItems">
+
+      <div className='item flex flex-col gap-2'>
+        <span className='text-gray-500'>Model Year</span>
+        <span className='font-bold'>{data?.modelYear}</span>
+      </div>
+
+      <div className='item flex flex-col gap-2'>
+        <span className='text-gray-500'>Category</span>
+        <span className='font-bold'>{data?.vehicleModel?.category?.name}</span>
+      </div>
+      <div className='item flex flex-col gap-2'>
+        <span className='text-gray-500'>Current Km</span>
+        <span className='font-bold'>{data?.kmDriven}</span>
+      </div>
+      </div>
+      <div className="parentItems">
+
+      <div className='item flex flex-col gap-2'>
+        <span className='text-gray-500'>Fuel Type</span>
+        <span className='font-bold'>{data?.fuelType}</span>
+      </div>
+      <div className='item flex flex-col gap-2'>
+        <span className='text-gray-500'>Plate Number</span>
+        <span className='font-bold'>{data?.palletNumber}</span>
+      </div>
+    </div>
+      </div>
+
+  </div>
   </>
    
 }
